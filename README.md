9c0c31c47f840fd24b62f6e86f2a4b6a16178597
golemz <golemz@163.com>
2020-12-01 09:35:24 +0800

    submit code.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # SimpleNTP
 Simple NTP，to synchronization time in a lan
-一个简单的时间同步工具，同步局域网内机器的时间，一台机器运行服务端，其他的运行客户端
+can run in Windows or Linux or MacOS
+one pc run as server, and others as client
+
+run as server: python3 sntp.py -s
+run as client: python3 sntp.py
+
+
+
+一个简单的时间同步工具，同步局域网内机器的时间，集服务器和客户端功能于一体，
+一台机器运行服务端，其他机器运行客户端
+未使用第三方库，兼容Windows和Linux和MacOS
+
 支持windows，linux，mac
+服务端运行：python3 sntp.py -s
+客户端运行：python3 sntp.py
---
+++ sntp/sntp.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+#coding:utf-8
+'''
+Simple time synchronizer, to synchronization time in a lan.
+
+
+usage: python3 sntp.py [-h] [-s] [-i interval]
+
+SimpleNtp server and client
+
+optional arguments:
+  -h, --help   show this help message and exit
+  -s           run as ntp server
+  -i interval  interval of broadcasting (default: 5 seconds)
+'''
+
+import socket, traceback, json
+import time, datetime, os, sys
+import argparse
+import ipaddress
+import socket
+
+ip  = socket.gethostbyname(socket.gethostname())
+net = ipaddress.IPv4Network(ip + '/24', False)
+ADDR = net.broadcast_address.compressed  # or just '192.168.1.255'
+PORT = 6666
+TIME_FORMAT = '%(year)s-%(month)s-%(day)s %(hour)s:%(minute)s:%(second)s.%(microsecond)s'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+localtimezone = -time.timezone//3600
+print('timezone:', localtimezone)
+isLinux = 'linux' in sys.platform
+
+def procCmdline():
+    '''command line'''
+    parser = argparse.ArgumentParser(description='SimpleNtp server and client', add_help=True)
+    parser.add_argument('-s', action='store_true', default=False, help='run as ntp server')
+    parser.add_argument('-i', type=int, default=5, metavar='interval', help='interval of broadcasting (default: 5 seconds)')
+    args = parser.parse_args()
+    args.i = args.i if args.i >= 1 else 5
+
+    if args.s:
+        server(args)
+    else:
+        client(args)
+
+def toTimeDict(curtime):
+    return {
+        'year' : curtime.year,
+        'month' : curtime.month,
+        #'weekday' :curtime.weekday()+1,
+        'day' : curtime.day,
+        'hour' : curtime.hour,
+        'minute' : curtime.minute,
+        'second' : curtime.second,
+        'microsecond' : curtime.microsecond
+    }
+
+def server(args):
+    print ("MyNtp Server. ")
+    print ("press Ctrl + c to stop ")
+
+    dest = (ADDR, PORT)
+    print ('Broadcasting time to %s every %d seconds'%(dest, args.i))
+
+    while True:
+        try:
+            curtime = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
+            timedict = toTimeDict(curtime)
+
+            data = {'time' : timedict}
+            msg = json.dumps(data)
+            s.sendto(msg.encode('utf-8'), dest)
+
+            curtime = curtime.astimezone(datetime.timezone(datetime.timedelta(hours=localtimezone)))
+            timedict = toTimeDict(curtime)
+            timestr = TIME_FORMAT % timedict
+            print('Broadcasting time: %s'%timestr)
+
+        except (KeyboardInterrupt, SystemExit):
+            return
+        except:
+            traceback.print_exc()
+
+        try:
+            time.sleep(args.i)
+        except (KeyboardInterrupt, SystemExit):
+            print('')
+            return
+
+def client(args):
+    print ("MyNtp Client. ")
+    print ("press Ctrl + c to stop ")
+
+    dest = ('', PORT)
+    s.bind(dest)
+    print ('Listening for broadcast at', s.getsockname())
+    while True:
+        try:
+            message, addr = s.recvfrom(8192)
+            data = json.loads(message.decode('utf-8'))
+            timedict = data['time']
+
+            curtime = datetime.datetime(**timedict).replace(tzinfo=datetime.timezone.utc)
+            curtime = curtime.astimezone(datetime.timezone(datetime.timedelta(hours=localtimezone)))
+
+            timedict = toTimeDict(curtime)
+            timestr = TIME_FORMAT % timedict
+            print('Got time: %s from %s'%(timestr, addr))
+
+            if isLinux:
+                os.system("date -s '%s';hwclock -w"%timestr)
+            else:
+                timedict['microsecond'] = str(timedict['microsecond'])[:2]  #for windows，only get first two number
+                os.system('time %(hour)s:%(minute)s:%(second)s.%(microsecond)s'%timedict)
+                os.system('date %(year)s-%(month)s-%(day)s'%timedict)
+
+        except (KeyboardInterrupt, SystemExit):
+            print('')
+            return
+        except:
+            traceback.print_exc()
+
+if __name__ == '__main__':
+    procCmdline()
